b2728ff03241802533ca20cb3b3cda3398d90df5
Phil Downey <Phil.Downey1@ibm.com>
2023-09-18 15:52:33 -0700

    Updates for installer critical environment fix

--- ad4e_opentoolkit/app/__init__.py
+++ ad4e_opentoolkit/app/__init__.py
@@ -1,11 +1,3 @@
 
-from . import grammar
-from . import help
-from . import plugins
-from . import lang_file_system
-from . import lang_mols
-from . import lang_runs
-from . import lang_sessions_and_registry
-from . import lang_workspaces
-from  ad4e_opentoolkit.app import global_var_lib as global_var_lib
-#from .. import global_var_lib as global_var_lib
+
+
