316af317a870320eb19843a3b6a9b4af5b700a04
Moenen Erbuer <moenen@arthur.io>
2023-10-09 12:23:56 +0300

    Output table fix

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas
-from ad4e_opentoolkit.helpers.output import output_table
-
-lst = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# df = pandas.DataFrame(lst, columns=['a', 'b', 'c'])
-output_table(lst, headers=['a', 'b', 'c'])
