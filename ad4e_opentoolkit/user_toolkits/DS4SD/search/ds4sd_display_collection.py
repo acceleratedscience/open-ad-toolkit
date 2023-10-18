bab08ee017f02c2e2e5d2a586ce7a5cdf246b428
Phil Downey <phildowney@pd-work-mac.local>
2023-10-19 08:29:12 +1100

    display collection pylint

--- ad4e_opentoolkit/user_toolkits/DS4SD/search/ds4sd_display_collection.py
+++ ad4e_opentoolkit/user_toolkits/DS4SD/search/ds4sd_display_collection.py
@@ -1,21 +1,13 @@
-import deepsearch as ds
-import numerize
-from deepsearch.cps.client.components.elastic import ElasticDataCollectionSource
-from deepsearch.cps.queries import DataQuery
-from deepsearch.cps.client.components.queries import RunQueryError
-from  ad4e_opentoolkit.helpers.output import output_table as output_table
+""" Displays a Domains Collections"""
+import pandas as pd
+from  ad4e_opentoolkit.helpers.output import output_table
 _tableformat = 'simple'
 
-
-# login
-
-# https://cps.foc-deepsearch.zurich.ibm.com/projects/1234567890abcdefghijklmnopqrstvwyz123456/library
-
 def display_collection(inputs: dict, cmd_pointer):
+    """ Displays a Domains Collections"""
     api = cmd_pointer.login_settings['toolkits_api'][cmd_pointer.login_settings['toolkits'].index('DS4SD') ]
     collections = api.elastic.list(domain=inputs['domain']['val'])
     collections.sort(key=lambda c: c.name.lower())
-
     results = [
         {
             "Name": c.name,
@@ -25,16 +17,9 @@ def display_collection(inputs: dict, cmd_pointer):
             "Coords": f"{c.source.elastic_id}/{c.source.index_key}",
         }
         for c in collections]
-    if cmd_pointer.notebook_mode == True:
-
-        import pandas as pd
+    if cmd_pointer.notebook_mode is  True:
         return pd.DataFrame(results)
     else:
-        from tabulate import tabulate
-        import pandas as pd
         collectives = pd.DataFrame(results)
-        head = ["Name", "Type", "Num entries", "Date", "Coords"]
-        #pdtabulate = tabulate(collectives, tablefmt=_tableformat, headers=head, showindex=False)
-        #print('\n' + pdtabulate + '\n')
         output_table(collectives,cmd_pointer,tablefmt=_tableformat   )
            
