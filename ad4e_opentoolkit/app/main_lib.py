5e908f6f71aa3bb658a923fa26a89e38d8e5ff41
Moenen Erbuer <moenen@arthur.io>
2023-10-24 20:17:32 +0300

    Removed unused code

--- ad4e_opentoolkit/app/main_lib.py
+++ ad4e_opentoolkit/app/main_lib.py
@@ -494,17 +494,6 @@ def display_data__open(cmd_pointer, parser, edit_mode=False):
     hash = '#edit' if edit_mode else ''
     launcher.launch(cmd_pointer, routes, 'dataviewer', hash=hash)
 
-    # # Parse dataframe.
-    # data = cmd_pointer.memory['data'].values.tolist()
-    # data = json.dumps(data)
-    # headers = cmd_pointer.memory['data'].columns.tolist()
-    # headers = json.dumps(headers)
-
-    # # Load routes and launch browser UI.
-    # routes = fetchRoutesDataViewer(data, headers)
-    # hash = '#edit' if edit_mode else ''
-    # launcher.launch(cmd_pointer, routes, 'dataviewer', hash=hash)
-
 
 # Edit a JSON config file.
 def edit_config(cmd_pointer, parser):
