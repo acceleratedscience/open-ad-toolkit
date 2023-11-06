b8b4f195a0f042437bd339c90b0c58659755e010
Phil Downey <phildowney@pd-work-mac.local>
2023-11-06 17:07:07 +1100

    rxn inerpret recipe update

--- ad4e_opentoolkit/user_toolkits/RXN/Reactions/rxn_recipe_interpret.py
+++ ad4e_opentoolkit/user_toolkits/RXN/Reactions/rxn_recipe_interpret.py
@@ -1,4 +1,5 @@
 """ Interprets a free text paragraph into a set of Recipe instructions """
+import os
 import importlib.util as ilu
 from ad4e_opentoolkit.helpers.output import output_text
 
@@ -18,19 +19,23 @@ def get_include_lib(cmd_pointer):
 
 def interpret_recipe(inputs: dict, cmd_pointer):
     """Interprets a free text paragraph into a set of Recipe instructions"""
-    receipe = inputs["receipe"]
+    recipe = inputs["receipe"]
     rxn4chemistry_wrapper = cmd_pointer.login_settings["client"][cmd_pointer.login_settings["toolkits"].index("RXN")]
     # Prepare the data query
-
+    if os.path.isfile(cmd_pointer.workspace_path(cmd_pointer.settings["workspace"].upper()) + "/"+recipe.strip()) is True:
+        output_text(f'Attempting to Open and Process Recipe in Workspace File: <green> {recipe} </green>',cmd_pointer=cmd_pointer,return_val=False)
+        with open(cmd_pointer.workspace_path(cmd_pointer.settings["workspace"].upper()) + "/"+recipe.strip(),'r',encoding='utf-8' ) as handle:
+            recipe= handle.read()
+   
     if cmd_pointer.notebook_mode is True:
         from IPython.display import Markdown  # pylint: disable=import-outside-toplevel
     try:
         return_result = []
-        actios_from_procedure_results = rxn4chemistry_wrapper.paragraph_to_actions(receipe)
+        actios_from_procedure_results = rxn4chemistry_wrapper.paragraph_to_actions(recipe)
         if cmd_pointer.notebook_mode is True:
-            return_result.append("***See the following actions from the Receipe:***\n")
+            return_result.append("***See the following actions from the Recipe:***\n")
         else:
-            return_result.append("See the following actions from the Receipe:\n")
+            return_result.append("See the following actions from the Recipe:\n")
 
         for index, action in enumerate(actios_from_procedure_results["actions"], 1):
             if cmd_pointer.notebook_mode is True:
@@ -40,7 +45,7 @@ def interpret_recipe(inputs: dict, cmd_pointer):
         if cmd_pointer.notebook_mode is True:
             return Markdown("".join(return_result))
         else:
-            return output_text("\n" + "".join(return_result), cmd_pointer=cmd_pointer)
+            return output_text("\n" + "".join(return_result)+"\n", cmd_pointer=cmd_pointer)
     except Exception as e:
         raise Exception(
             "unable to to turn paragraph to actions:" + str(e)
