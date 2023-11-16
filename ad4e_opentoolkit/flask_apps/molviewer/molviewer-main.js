65680751039c211ba4833007a492e28273db38ce
Moenen Erbuer <moenen@arthur.io>
2023-11-16 19:00:20 +0200

    Swapped out 3dmol with jsmol, which renders the molecules more correctly

--- ad4e_opentoolkit/flask_apps/molviewer/molviewer-main.js
+++ ad4e_opentoolkit/flask_apps/molviewer/molviewer-main.js
@@ -1,8 +1,20 @@
 render3dMol()
 populateMolData(document.getElementById('data-inchi').innerText)
 
+//
+//
+
+/**
+ * Render the molecule in 3D.
+ */
 function render3dMol() {
 	const $wrap = document.querySelector('#mol-render .mol-3d')
+	const inchi = document.getElementById('data-inchi').innerText
+	_render3dMol_Jmol($wrap, inchi)
+}
+
+// Using the 3DMol library - https://3dmol.org
+function _render3dMol_3DMol() {
 	// let config = { backgroundColor: '#333' }
 	const config = { backgroundColor: '#000' }
 	const viewer = $3Dmol.createViewer($wrap, config)
@@ -12,6 +24,7 @@ function render3dMol() {
 	viewer.addModel(mol_sdf, 'sdf')
 
 	// Check styling options: https://3dmol.org/tests/example.html
+	// More advances visualizing: https://3dmol.org/viewer.html?pdb=1YCR&select=chain:A&style=cartoon;stick&select=chain:B&style=line;sphere&select=resi:19,23,26;chain:B&style=stick;sphere&select=resi:19,23,26;chain:B&labelres=backgroundOpacity:0.8;fontSize:14
 	viewer.setStyle({}, { stick: {} })
 	// viewer.setStyle({}, { sphere: {} })
 	// viewer.setStyle({ hetflag: false }, { cartoon: {} })
@@ -33,15 +46,40 @@ function render3dMol() {
 	}
 }
 
-// Update the DOM with PubChem data.
+// Using the Jmol library - https://jmol.sourceforge.net
+function _render3dMol_Jmol($wrap, inchi) {
+	const molInfo = {
+		width: '100%',
+		height: '100%',
+		debug: false,
+		j2sPath: 'app/jsmol/j2s',
+		color: '0xC0C0C0',
+		disableJ2SLoadMonitor: true,
+		disableInitialConsole: true,
+		appletLoadingImage: 'none',
+		addSelectionOptions: false,
+		serverURL: 'app/jsmol/jsmol.php',
+		use: 'HTML5',
+		readyFunction: null,
+		// script: 'load $' + smiles,
+		script: 'load "$' + inchi + '"; background "#f6f6f6";', //  zoom 70;
+	}
+
+	$wrap.innerHTML = Jmol.getAppletHtml('jmolApplet1', molInfo)
+}
+
+/**
+ * Update the DOM with PubChem data.
+ * @param {String} inchi InChI string which will be used to fetch the PubChem data.
+ */
 async function populateMolData(inchi) {
 	let mol_json = document.getElementById('mol-data').getAttribute('data-json')
 	mol_json = JSON.parse(mol_json)
-	const cid = await fetchMolCID(inchi)
+	const cid = await _fetchMolCID(inchi)
 	if (cid) {
 		mol_json['cid'] = cid
-		const pubchem_data = await fetchMolData(cid)
-		const pubchem_data_simple = parsePubChem(pubchem_data)
+		const pubchem_data = await _fetchMolData(cid)
+		const pubchem_data_simple = _parsePubChem(pubchem_data)
 		mol_json.pubChem = pubchem_data_simple
 
 		/**
@@ -83,7 +121,44 @@ async function populateMolData(inchi) {
 	}
 }
 
-function parsePubChem(pubchem_data) {
+// Fetch the CID from PubChem.
+async function _fetchMolCID(inchi) {
+	try {
+		// https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest-tutorial#section=Special-Characters-in-the-URL
+		const response = await fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchi/cids/JSON?inchi=${encodeURIComponent(inchi)}`)
+		if (!response.ok) {
+			throw new Error('fetchMolCID() PubChem PUG REST API response is not ok')
+		}
+		const data = await response.json()
+		const cid = 'IdentifierList' in data && 'CID' in data.IdentifierList ? data.IdentifierList.CID[0] : null
+		if (!cid) {
+			const err = Fault in data && Details in data.Fault && output in data.Fault.Details ? '\n' + data.Fault.Details.output : ''
+			throw new Error('fetchMolCID() failed to extract cid from API response', err)
+		}
+		return cid
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+// Fetch all molecule data from PubChem.
+async function _fetchMolData(cid) {
+	try {
+		// https://pubchem.ncbi.nlm.nih.gov/docs/pug-view
+		const response = await fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/${cid}/JSON`)
+		if (!response.ok) {
+			throw new Error('fetchMolData() PubChem PUG View API response is not ok')
+		}
+		const data = await response.json()
+		return data
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+// Crawl through the huge pubchem data object and extract the relevant data.
+// To see the full profile for ibuprofen: https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/3672/JSON
+function _parsePubChem(pubchem_data) {
 	const params = {}
 
 	const sections = pubchem_data.Record.Section
@@ -150,39 +225,4 @@ function parsePubChem(pubchem_data) {
 		}
 	})
 	return params
-}
-
-// Fetch the CID from PubChem.
-async function fetchMolCID(inchi) {
-	try {
-		// https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest-tutorial#section=Special-Characters-in-the-URL
-		const response = await fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchi/cids/JSON?inchi=${encodeURIComponent(inchi)}`)
-		if (!response.ok) {
-			throw new Error('fetchMolCID() PubChem PUG REST API response is not ok')
-		}
-		const data = await response.json()
-		const cid = 'IdentifierList' in data && 'CID' in data.IdentifierList ? data.IdentifierList.CID[0] : null
-		if (!cid) {
-			const err = Fault in data && Details in data.Fault && output in data.Fault.Details ? '\n' + data.Fault.Details.output : ''
-			throw new Error('fetchMolCID() failed to extract cid from API response', err)
-		}
-		return cid
-	} catch (err) {
-		console.error(err)
-	}
-}
-
-// Fetch all molecule data from PubChem.
-async function fetchMolData(cid) {
-	try {
-		// https://pubchem.ncbi.nlm.nih.gov/docs/pug-view
-		const response = await fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/${cid}/JSON`)
-		if (!response.ok) {
-			throw new Error('fetchMolData() PubChem PUG View API response is not ok')
-		}
-		const data = await response.json()
-		return data
-	} catch (err) {
-		console.error(err)
-	}
 }
