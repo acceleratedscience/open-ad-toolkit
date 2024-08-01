6b6d89a7caf99e8ae21434497d8cafa4b3967329
Phil Downey <phildowney@pd-work-mac.local>
2024-08-02 08:39:09 +1000

    update for continer proxy base_url

--- openad/gui-build-proxy/assets/JsonViewer-CTx4TCsR.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./JsonViewer.vue_vue_type_script_setup_true_lang-DN3J3yM9.js";import"./index-DbH5AzoW.js";import"./BreadCrumbs-DYYYzGiH.js";import"./TextBox-BI15P2yr.js";export{o as default};
